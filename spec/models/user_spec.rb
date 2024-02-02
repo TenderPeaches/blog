decfd18bff4794671725d5864e69c9588129e170
Poubelle <tendrepeches@protonmail.com>
2024-02-01 23:33:47 -0500

    user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe User, type: :model do
     # same as `expect(user.valid?).to eq(true)`
     expect(user).to be_valid
   end
+
+  # use `describe` to group specs into logical units
+  describe 'validations' do
+    # shoulda-matchers
+    # name attribute validations
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+    # email attribute validations
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+    # email sanity checks
+    it { is_expected.to_not allow_value('foo@').for(:email) }
+    it { is_expected.to_not allow_value('@bar.com').for(:email) }
+  end
 end
