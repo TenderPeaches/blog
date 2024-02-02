73a98893813d682b33b69055849cbf2614224e46
Poubelle <tendrepeches@protonmail.com>
2024-02-01 23:16:30 -0500

    working RSpec & first tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # use `FactoryBot.build` to create a new user instance
+  # `let` is lazily evaluated
+  let(:user) { build(:user) }
+
+  # ensure the factory produces a valid model instance
+  it 'has a valid factory' do
+    # same as `expect(user.valid?).to eq(true)`
+    expect(user).to be_valid
+  end
 end
